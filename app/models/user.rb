cb93ac7017ee392df12f2a587225b8cd9b39a033
NobuMuto <tobe.is.todo1214@gmail.com>
2021-02-11 16:51:03 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, :encrypted_password, :nickname, :first_name, :last_name, :first_name_kana, :first_name_kana, :last_name_kana, presence: true
 
-         
 end
