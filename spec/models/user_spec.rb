049218930943ff605027674819ff67efb2da3d0c
NobuMuto <tobe.is.todo1214@gmail.com>
2021-02-19 23:26:07 +0900

    テストコードとバリデーション

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,37 +7,105 @@ RSpec.describe User, type: :model do
 
   describe '新規登録/ユーザー情報' do
     it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@user).to be_valid
     end
     it 'nicknameが6文字以下であれば登録できる' do
+      @user.nickname = "aaaaaa"
+      expect(@user).to be_valid
     end
     it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      @user.password = "000000"
+      @user.password_confirmation = "000000"
+      expect(@user).to be_valid
     end
     it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空では登録できない' do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation  doesn't match Password")
     end
     it 'nicknameが7文字以上では登録できない' do
+      @user.nickname = "aaaaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 6 characters)")
     end
     it '重複したemailが存在する場合登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
     it 'passwordが5文字以下では登録できない' do
+      @user.password = "00000"
+      @user.password_confirmation = "00000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
   end
 
   describe '新規登録/本人情報確認' do
     it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First_name can't be blank")
+
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last_name can't be blank")
     end
     it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+      @user.first_name = "aaaa"
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include("First_name ")
+
+      @user.last_name = "aaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last_name ")
+
     end
     it 'ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること' do
+      @user.first_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+
+      @user.last_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last_name_kana can't be blank")
     end
     it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
+
+      @user.first_name_kana = "あ"
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include()
+
+      @user.last_name_kana = "い"
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include()
+
+
     end
     it '生年月日が必須であること' do
+      @user.birth_day = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth_day can't be blank")
     end
   end
 end
