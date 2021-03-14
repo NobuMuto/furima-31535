f2d505f1dee4bcfe4e114afafccdc14012898dbd
NobuMuto <tobe.is.todo1214@gmail.com>
2021-03-14 16:13:11 +0900

    バリデーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence:true do
+    validate :name
+    validate :detail
+    validate :image
     validate :category
     validate :status
     validate :delivery_fee
     validate :delivery_area
     validate :delivery_date
+    validate :price
   end
 
   belongs_to :category
@@ -15,5 +19,8 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :delivery_area
   belongs_to :delivery_date
+
+  validates :price, format: { with: /\A[0-9]\z/, message: '半角数字を使用してください' } 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
 
 end
