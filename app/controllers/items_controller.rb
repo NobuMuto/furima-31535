fee6f0644306632be423b26554d05f5d719dbea1
NobuMuto <tobe.is.todo1214@gmail.com>
2021-03-14 17:19:10 +0900

    表示、分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   #def move_to_index
   #  unless user_signed_in?
   #    redirect_to action: :index
@@ -12,8 +25,8 @@ class ItemsController < ApplicationController
   
   private
 
-  #def item_params
-  #  params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  #end
+  def item_params
+    params.require(:item).permit(:name, :detail, :image, :category, :status, :delivery_fee, :delivery_area, :delivery_date, :price)#.merge(user_id: current_user.id)
+  end
 
 end
