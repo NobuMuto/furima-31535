b5e28537622b3457adeba63c4ddd7b1efeedd712
NobuMuto <tobe.is.todo1214@gmail.com>
2021-03-20 15:46:30 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,11 @@ class Item < ApplicationRecord
     validates :name
     validates :detail
     validates :image
-    validates :category_id
-    validates :status_id
-    validates :delivery_fee_id
-    validates :delivery_area_id
-    validates :delivery_date_id
+    #validates :category_id
+    #validates :status_id
+    #validates :delivery_fee_id
+    #validates :delivery_area_id
+    #validates :delivery_date_id
     validates :price
   end
 
