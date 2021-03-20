84991efb6c3c2f3b0a68deb0f77c64645d450763
NobuMuto <tobe.is.todo1214@gmail.com>
2021-03-20 13:21:23 +0900

    before_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
   end
@@ -18,11 +18,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def move_to_index
-  #  unless user_signed_in?
-  #    redirect_to action: :index
-  #  end
-  # end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
   private
 
