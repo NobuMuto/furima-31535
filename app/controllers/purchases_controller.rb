ac2d02c34164a7dcbadea2b88658b7761dfa8319
NobuMuto <tobe.is.todo1214@gmail.com>
2021-04-04 17:37:18 +0900

    クレジット機能のJS、コントローラーの記載。秘密鍵の設定。

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,8 +4,9 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @purchase_post = PurchasePost.new(purchase_params)   #「UserDonation」に編集
+    @purchase_post = PurchasePost.new(purchase_post_params)   #「UserDonation」に編集
      if @upurchase_post.valid?
+      pay_item
        @purchase_post.save
        redirect_to action: :index
      else
@@ -15,8 +16,16 @@ class PurchasesController < ApplicationController
   
   private
 
-  def purchase_params
-    params.require(:purchase_post).permit(:user_id, :item_id, :postal_code, :delivery_area_id, :city, :number, :building, :tel_number, :purchase_id, :token)
+  def purchase_post_params
+    params.require(:purchase_post).permit(:user_id, :item_id, :postal_code, :delivery_area_id, :city, :number, :building, :tel_number, :purchase_id).merge(token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]    
+    Payjp::Charge.create(
+      amount: purchase_post_params[:price],  
+      card: purchase_post_params[:token],    
+      currency: 'jpy'                 
+    )
+  end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,36 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);  // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_post[number]"),
+      cvc: formData.get("purchase_post[cvc]"),
+      exp_month: formData.get("purchase_post[exp_month]"),
+      exp_year: `20${formData.get("purchase_post[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("purchase_post_number").removeAttribute("name");
+      document.getElementById("purchase_post_cvc").removeAttribute("name");
+      document.getElementById("purchase_post_exp_month").removeAttribute("name");
+      document.getElementById("purchase_post_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
 };
 
 window.addEventListener("load", pay);
